7d490a65c25cd0963ffd1e9e6252815acdbeeb23
Billy Joe <as478277813@gmail.com>
2020-12-14 21:18:49 +0800

    first commit 12/14 update version1.011

--- src/main/java/Game/client.java
+++ src/main/java/Game/client.java
@@ -4,5 +4,6 @@ public class client {
     public static void main(String[] args) {
         System.out.println("Tank War!");
         System.out.println("client");
+        System.out.println("practice");
     }
 }
